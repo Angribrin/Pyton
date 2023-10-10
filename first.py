c9c8f4b9ec1ee3312c9ef6fee33eb84ecbb1fa6b
Angribrin <73384533+Angribrin@users.noreply.github.com>
2023-10-10 18:27:16 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,1 +1,18 @@
+import random
 
+user_name = input("What is youre name?\n")
+
+number = random.randint(1,20)
+counter = 0
+
+while True:
+    guess_number = int(input("Enter youre number: "))
+    if guess_number == number :
+        print("you are the winner")
+        break
+    elif 1<=guess_number<=20 and guess_number<number:
+        print("Youre number is bigger")
+    elif 1<=guess_number<=20 and guess_number>number:
+        print("Youre number is less")
+    elif not 1<=guess_number<=20:
+        print("Yore number is not in the range")
